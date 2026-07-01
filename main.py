fff55bcdb244d29a6b05ebdd378836b35fecdd57
Arihant Tyagi <arihanttyagi78@gmail.com>
2026-07-01 16:54:01 +0100

    Added CLI interface to the main.py script for better user interaction and functionality.

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ from src.translator import translate_text
 
 def main():
     print("Welcome to the Language Translator")
-    text = input("Enter text: ").strip()
-    destination = input("Target Language (en,es,fr...): ").strip().lower()
-    result = translate_text(text, destination)
-    print(f"\nTranslation: {result}")
 
+    while True:
+        print("\n1. Translate Text")
+        print("2. Exit")
+        choice = input("\n> ").strip()
+
+        if choice == "1":
+            text = input("Enter text: ").strip()
+            if not text:
+                print("Error: Text cannot be empty.")
+                continue
+            
+            destination = input("Target Language (en,es,fr...): ").strip().lower()
+            if not destination:
+                print("Error: need a language code.")
+                continue
+    
+            result = translate_text(text, destination)
+            print(f"\nTranslation: {result}")
+        elif choice == "2":
+            print("Exiting the translator. Bye!")
+            break
+        else:
+            print(f"'{choice}' isn't a valid option.")
 if __name__ == "__main__":
     main()
